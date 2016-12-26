edfa56d849f375f82a72b0e909663a48788aaa65
alokdixit86 <alok.dixit@3pillarglobal.com>
2016-12-26 13:18:18 +0530

    Alok's Practice work

---
+++ AlokPersonalProject/src/oops/Cube.java
@@ -0,0 +1,27 @@
+package oopd;
+
+public class Cube {
+int length;
+int breadth;
+int height;
+
+Cube()
+{
+	length=10;
+	breadth=20;
+	height=30;
+}
+
+public Cube(int i, int j, int k) {
+	length=i;
+	breadth=j;
+	height=k;
+}
+
+public int getVolumeOfCube()
+{
+return(length * breadth * height);
+}
+	
+}
+
