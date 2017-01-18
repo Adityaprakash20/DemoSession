f15955f2af3bb2cbbb36e8ca13d87049177f439a
vikash373 <vikash373@live.com>
2017-01-18 16:28:55 +0530

    Updated hash map imlementation class on 18/01/2017

--- src/testproject1/HashMapImlementation.java
+++ src/testproject1/HashMapImlementation.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class HashMapImlementation{
 	
 	public static HashMap<Integer, Employee> hm;
-	
 	public static int heid;
 	public static String hename;
 	public static String heqa;
@@ -16,22 +15,22 @@ public class HashMapImlementation{
 
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 		hm = new HashMap<Integer, Employee>() ;
 		Scanner reader = new Scanner(System.in);
-				
+		Employee emp1 = new Employee();
+		
 		for(int i=1;i<=1;i++){
 			
 			// Reading from System.in
 			System.out.println("Enter employee details for employee "+ i +":");
 			System.out.println("Employee Name");
-			hename = reader.next(); 
+			emp1.setEname(reader.next());  
 			System.out.println("Employee ID (Only Numeric)");
-			heid = reader.nextInt(); 
+			emp1.setEmpid(reader.nextInt()); 
 			System.out.println("Employee Qualification");
-			heqa =  reader.next();  
-			Employee emp1 = new Employee(hename, heid, heqa);
+			emp1.setEqualification(reader.next());
+			
 			hm.put(i, emp1 );			
 			
 		}
@@ -39,7 +38,7 @@ public class HashMapImlementation{
 		System.out.println("EmpID"+"   "+"EmpName"+"   "+"EmpQualifcation");
 		System.out.println("---------------------------------------");
 		for(int i=1;i<=hm.size();i++){	
-		System.out.println(hm.get(i).empid + "       " +hm.get(i).ename + "      " +hm.get(i).equalification);
+		System.out.println(hm.get(i).getEname() + "       " +hm.get(i).getEmpid() + "      " +hm.get(i).getEqualification());
 		System.out.println();
 		}
 
